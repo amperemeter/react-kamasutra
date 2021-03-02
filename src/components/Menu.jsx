960560cacd7b2e35846363d2c1c7e69d1bcb1b52
Darya <daryakushina@gmail.com>
2021-03-02 17:02:50 +0300

    Add all my files

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Menu = () => {
+   return (
+      <nav className="app-menu">
+         <div>Profile</div>
+         <div>Messages</div>
+         <div>News</div>
+         <div>Music</div>
+         <div>Settings</div>
+      </nav>
+   )
+}
+
+export default Menu
