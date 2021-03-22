7a473fca2d487e3deef006cf357bcc2e4b11acb2
Amperemeter <daryakushina@gmail.com>
2021-03-22 16:15:46 +0300

    state

--- src/redux/state.js
+++ src/redux/state.js
@@ -25,19 +25,20 @@ let store = {
    _callSubscriber() {
       console.log('State was changed');
    },
-   addPost() {
-      let newPost = {
-         id: this._state.profilePage.posts.length + 1,
-         message: this._state.profilePage.newPostText,
-         likesCount: 0,
-      };
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubscriber(this._state);
-   },
-   updateNewPostText(newText) {
-      this._state.profilePage.newPostText = newText;
-      this._callSubscriber(this._state);
+   dispatch(action) {
+      if (action.type === 'ADD-POST') {
+         let newPost = {
+            id: this._state.profilePage.posts.length + 1,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+         };
+         this._state.profilePage.posts.push(newPost);
+         this._state.profilePage.newPostText = "";
+         this._callSubscriber(this._state);
+      } else if (action.type === 'UPDAT-NEW-POST-TEXT') {
+         this._state.profilePage.newPostText = action.newText;
+         this._callSubscriber(this._state);
+      }
    },
    subscriber(observer) {
       this._callSubscriber = observer;
