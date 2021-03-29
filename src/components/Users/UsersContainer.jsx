0dfb3dcfa2b98c0742cb16a296a5746be8406b00
Amperemeter <daryakushina@gmail.com>
2021-03-29 18:35:19 +0300

    55

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,10 +1,13 @@
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import { followAC, setCurrentPageAC, setUsersAC, unfollowAC, setTotalUsersCountAC } from "../../redux/users-reducer";
 import Users from "./Users";
 
 const mapStateToProps = (state) => {
    return {
-      users: state.usersPage.users
+      users: state.usersPage.users,
+      pageSize: state.usersPage.pageSize,
+      totalUsersCount: state.usersPage.totalUsersCount,
+      currentPage: state.usersPage.currentPage,
    }
 }
 
@@ -18,7 +21,13 @@ const mapDispatchToProps = (dispatch) => {
       },
       unfollow: (userId) => {
          dispatch(unfollowAC(userId))
-      }
+      },
+      setCurrentPage: (pageNumber) => {
+         dispatch(setCurrentPageAC(pageNumber))
+      },
+      // setTotalUsersCount: (totalCount) => {
+      //    dispatch(setTotalUsersCountAC(totalCount))
+      // }
    }
 }
 
