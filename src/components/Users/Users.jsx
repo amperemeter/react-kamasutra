a705225064a483a53ff2b71def031bb9224f9216
Amperemeter <daryakushina@gmail.com>
2021-03-31 15:52:09 +0300

    62

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./Users.module.css";
 import userPhoto from '../../assets/images/avatar.png';
 import { NavLink } from "react-router-dom";
+import * as axios from 'axios';
 
 let Users = (props) => {
    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -30,8 +31,37 @@ let Users = (props) => {
                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={s.image} />
                         </NavLink>
                         {!u.followed
-                           ? <button onClick={() => { props.follow(u.id) }}>Follow</button>
-                           : <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                           ? <button onClick={() => {
+                              axios.post(
+                                 `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                                 null,
+                                 {
+                                    withCredentials: true,
+                                    headers: {
+                                       'API-KEY': '085e3771-2e67-4379-8386-bf92ed5e2175'
+                                    }
+                                 })
+                                 .then(response => {
+                                    if (response.data.resultCode === 0) {
+                                       props.follow(u.id)
+                                    }
+                                 })
+                           }}>Follow</button>
+                           : <button onClick={() => {
+                              axios.delete(
+                                 `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                                 {
+                                    withCredentials: true,
+                                    headers: {
+                                       'API-KEY': '085e3771-2e67-4379-8386-bf92ed5e2175'
+                                    }
+                                 })
+                                 .then(response => {
+                                    if (response.data.resultCode === 0) {
+                                       props.unfollow(u.id)
+                                    }
+                                 })
+                           }}>Unfollow</button>
                         }
 
                      </div>
@@ -51,7 +81,7 @@ let Users = (props) => {
                )
             })
          }
-      </div>
+      </div >
    )
 }
 
