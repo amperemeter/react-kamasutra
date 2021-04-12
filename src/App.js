4ffba2fc0d3fca6dec16ffc0bf6e1eee23af2fd2
Amperemeter <daryakushina@gmail.com>
2021-04-12 17:31:28 +0300

    80

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
+import { compose } from 'redux';
+import { getAuthUserData } from './redux/auth-reducer';
 import Menu from './components/Menu/Menu';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
+import './App.css';
 
-function App() {
+class App extends React.Component {
+  componentDidMount() {
+    this.props.getAuthUserData();
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app-wrapper">
+          <HeaderContainer />
+          <Menu />
+          <main className="app-content">
+            <Route
+              path="/profile/:userId?"
+              render={() => <ProfileContainer />}
+            />
 
-  return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Menu />
-        <main className="app-content">
-          <Route
-            path="/profile/:userId?"
-            render={() => <ProfileContainer />}
-          />
+            <Route
+              path="/dialogs"
+              render={() => <DialogsContainer />}
+            />
 
-          <Route
-            path="/dialogs"
-            render={() => <DialogsContainer />}
-          />
-
-          <Route
-            path="/users"
-            render={() => <UsersContainer />}
-          />
-          <Route
-            path="/login"
-            render={() => <Login />}
-          />
-        </main>
-      </div>
-    </BrowserRouter>
-  );
+            <Route
+              path="/users"
+              render={() => <UsersContainer />}
+            />
+            <Route
+              path="/login"
+              render={() => <Login />}
+            />
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+export default compose(
+  withRouter,
+  connect(null, { getAuthUserData }))(App);
