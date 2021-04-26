2bb8313bc2e91d6438922b65d2a7b334a0cb0b64
Amperemeter <daryakushina@gmail.com>
2021-04-26 13:30:43 +0300

    auth profile

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,5 @@
-import { authAPI } from '../api/api';
+import { authAPI, profileAPI } from '../api/api';
+import { setUserProfile, setUserStatus } from './profile-reducer';
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
@@ -25,12 +26,26 @@ const authReducer = (state = initialState, action) => {
 export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } });
 
 export const getAuthUserData = () => (dispatch) => {
+   let userId = null;
    authAPI.me()
       .then(response => {
          if (response.data.resultCode === 0) {
             let { id, email, login } = response.data.data;
+            userId = response.data.data.id
             dispatch(setAuthUserData(id, email, login));
          }
+      })
+      .then(() => {
+         profileAPI.getProfile(userId)
+            .then(response => {
+               dispatch(setUserProfile(response.data));
+            });
+      })
+      .then(() => {
+         profileAPI.getStatus(userId)
+            .then(response => {
+               dispatch(setUserStatus(response.data));
+            });
       })
 }
 
